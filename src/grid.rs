45ab2c681714b385ea0ae6bfb5e45f0cd1899ecb
Sam Morrell <samuelmorrell@gmail.com>
2023-12-14 23:00:41 +0000

    2023 Day 14: Added transpose function for grids.

--- src/grid.rs
+++ src/grid.rs
@@ -64,3 +64,19 @@ pub fn find_coords_for<T: Eq + PartialEq>(haystack: &Vec<Vec<T>>, needle: T) -> 
         indices
     }).flatten().collect()
 }
+
+/// Transposes the vec of Vecs.
+/// Thanks to https://stackoverflow.com/a/64499219. 
+pub fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    assert!(!v.is_empty());
+    let len = v[0].len();
+    let mut iters: Vec<_> = v.into_iter().map(|n| n.into_iter()).collect();
+    (0..len)
+        .map(|_| {
+            iters
+                .iter_mut()
+                .map(|n| n.next().unwrap())
+                .collect::<Vec<T>>()
+        })
+        .collect()
+}
